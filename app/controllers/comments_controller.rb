3dc40df20565ce9a9f9e6177fc5c73874afcf384
Steve Bussert <strang3tim@Gmail.com>
2015-07-10 20:06:55 -0700

    fix comment username errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,13 +8,17 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
+    binding.pry
     if current_user
-      @comment[:user_id] = current_user.id
+      @comment[:username] = current_user.username
+    elsif params[:comment][:username] == "" || params[:comment][:username] == nil
+      @comment[:username] = "anonymous"
     else
-      @comment[:user_id] = 1
+      @comment[:username] = params[:comment][:username]
     end
+
     if @comment.save
-      redirect_to post_comment_path(@post, @comment)
+      redirect_to post_path(@post, @comment)
     else
       redirect_to :back
     end
@@ -22,7 +26,7 @@ class CommentsController < ApplicationController
 
   private
 
-  def answer_params
-    params.require(:answer).permit(:text)
+  def comment_params
+    params.require(:comment).permit(:username, :text)
   end
 end
